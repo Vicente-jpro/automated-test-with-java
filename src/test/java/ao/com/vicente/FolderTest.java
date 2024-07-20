d2df0ba98abb8deb88a360a2613dca499a5065fa
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-07-20 21:37:23 +0100

    Display name added

--- src/test/java/ao/com/vicente/FolderTest.java
+++ src/test/java/ao/com/vicente/FolderTest.java
@@ -3,40 +3,50 @@ package ao.com.vicente;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Model SimpleMath tests")
 class FolderTest {
 
+	Double n1 = 4D;
+	Double n2 = 4D;
+	
+	//
 	@Test
-	void soma() {
-		Double result = SimpleMath.soma(4D, 4D);
+	@DisplayName("It should sum two double numbers.")
+	void itShouldSumTwoNumbers() {
+		Double result = SimpleMath.soma(n1, n2);
 		Double expected = 8D;
 		// Message should display if the text fail.
-		assertEquals(result, expected, () -> "It should sum two double numbers");
+		assertEquals(result, expected);
 		assertNotNull(result);
 	}
 	
 	@Test
-	void multiplicao() {
-		Double result = SimpleMath.multipicacao(4D, 4D);
+	@DisplayName("It should multiply two double numbers.")
+	void itShouldMultiplyTwoNumbers() {
+		Double result = SimpleMath.multipicacao(n1, n2);
 		Double expeted = 16D;
-		assertEquals(result, expeted, () -> "It should divid two double numbers");
+		assertEquals(result, expeted);
 		assertNotNull(expeted);
 	}
 	
 	@Test
-	void divisao() {
-		Double result = SimpleMath.divisao(4D, 4D);
+	@DisplayName("It should divid two numbers.")
+	void itShouldDividTwoNumbers() {
+		Double result = SimpleMath.divisao(n1, n2);
 		Double expeted = 1D;
 		assertEquals(result, expeted);
 		assertNotNull(result);
 	}
 
 	@Test
-	void restoDivisao() {
-		Double result = SimpleMath.restoDivisao(4D, 4D);
+	@DisplayName("It should get the rest of division bwetween two numbers.")
+	void itShouldGetTheRestOfDivisionOfTwoNumbers() {
+		Double result = SimpleMath.restoDivisao(n1, n2);
 		Double expeted = 0D;
-		assertEquals(result, expeted);
+		assertEquals(result, expeted, ()-> "It should get rest of division.");
 		assertNotNull(result);
 	}
 
